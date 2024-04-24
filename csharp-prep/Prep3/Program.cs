29bce00023c9382bb10e0a6471e7e82654c6fa49
guyzoch101 <tristanmanhei@gmail.com>
2024-04-24 17:08:33 -0600

    Prep3 Stretch completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,25 +5,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magic_number = randomGenerator.Next(1, 100);
-        
-        int guess_number;
+        string status;
         do {
+            Random randomGenerator = new Random();
+            int magic_number = randomGenerator.Next(1, 100);
+        
+            int attempt = 0;
+            int guess_number;
+            do {
 
-            Console.Write("What is your guess? ");
-            string guess = Console.ReadLine();
-            guess_number = int.Parse(guess);
+                Console.Write("What is your guess? ");
+                string guess = Console.ReadLine();
+                guess_number = int.Parse(guess);
 
-            if (magic_number > guess_number) {
-                Console.WriteLine("Higher");
-            }
-            else if (magic_number < guess_number) {
-                Console.WriteLine("Lower");
-            }
+                if (magic_number > guess_number) {
+                    Console.WriteLine("Higher");
+                }
+                else if (magic_number < guess_number) {
+                    Console.WriteLine("Lower");
+                }
 
-        } while (magic_number != guess_number);
+                attempt += 1;
 
-        Console.WriteLine("You guessed it!");
+            } while (magic_number != guess_number);
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"Attempts used: {attempt}");
+        
+            Console.Write("Continue? (yes / no) ");
+            status = Console.ReadLine();
+            
+        } while (status == "yes");
     }
 }
