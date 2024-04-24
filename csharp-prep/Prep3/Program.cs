0f38d5394c38291f07ac69fb067760e74d83d267
guyzoch101 <tristanmanhei@gmail.com>
2024-04-24 17:03:51 -0600

    Prep3 Core completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+        
+        int guess_number;
+        do {
+
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guess_number = int.Parse(guess);
+
+            if (magic_number > guess_number) {
+                Console.WriteLine("Higher");
+            }
+            else if (magic_number < guess_number) {
+                Console.WriteLine("Lower");
+            }
+
+        } while (magic_number != guess_number);
+
+        Console.WriteLine("You guessed it!");
     }
 }
