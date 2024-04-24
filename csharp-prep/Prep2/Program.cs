e301f2a96aa8cbba46d5811ecf8c8b5ea3ecccf3
guyzoch101 <tristanmanhei@gmail.com>
2024-04-24 16:39:14 -0600

    Prep2 Core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string score_str = Console.ReadLine();
+
+        /* converting the returned string into an integer */
+        int score_int = int.Parse(score_str);
+
+        string grade;
+        if (score_int >= 90) {
+            grade = "A";
+        }
+        else if (score_int >= 80 && score_int < 90) {
+            grade = "B";
+        }
+        else if (score_int >= 70 && score_int < 80) {
+            grade = "C";
+        }
+        else if (score_int >= 60 && score_int < 70) {
+            grade = "D";
+        }
+        else {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade: {grade}");
+
+        if (score_int >= 70) {
+            Console.WriteLine("Congratulations! You have passed the class.");
+        }
+        else {
+            Console.WriteLine("Work harder! You will pass next time.");
+        }
     }
 }
