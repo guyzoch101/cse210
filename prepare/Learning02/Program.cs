351fd92d77c291b54c6e6729d4f0ad569e533e57
guyzoch101 <tristanmanhei@gmail.com>
2024-04-27 13:57:12 -0600

    minor amendmants

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -15,7 +15,7 @@ class Program
         job1._startYear = 2019;
         job1._endYear = 2022;
 
-        job1.Display();
+        job1.DisplayJobDetails();
 
         // creating a new job card with the class
         Job job2 = new Job();
@@ -26,7 +26,7 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        job2.Display();
+        job2.DisplayJobDetails();
 
         // displaying the company names
         Console.WriteLine(job1._company);
