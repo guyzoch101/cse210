24e7d45c49fca986dc24b3cda55e75c3a1880770
guyzoch101 <tristanmanhei@gmail.com>
2024-04-27 13:48:36 -0600

    added job1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // creating a new job card with the class
+        Job job1 = new Job();
+
+        // initializing the variables
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job1.Display();
     }
 }
