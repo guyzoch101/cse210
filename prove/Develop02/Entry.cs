81f57b9f9e1fb6991d1819f4f26d89f32c7c1cd9
guyzoch101 <tristanmanhei@gmail.com>
2024-05-02 00:11:01 -0600

    Added content for display function

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -4,6 +4,7 @@ public class Entry {
     public string _entryText;
     
     public void Display() {
-
+        Console.WriteLine($"{_date} : {_promptText}");
+        Console.WriteLine($"{_entryText}");
     }
 }
