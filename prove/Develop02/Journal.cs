8053a8a34ad6499130a9ebded74a090d75fdeb28
guyzoch101 <tristanmanhei@gmail.com>
2024-05-02 23:20:47 -0600

    basic content for the functions

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -2,18 +2,28 @@ public class Journal {
     public List<Entry> _entries = new List<Entry>();
 
     public void AddEntry(Entry newEntry) {
-
+        _entries.Add(newEntry);
     }
 
     public void DisplayAll() {
-
+        foreach (Entry entry in _entries) {
+            entry.Display();
+        }
     }
 
-    public void SaveToFile(string file) {
-
+    public void SaveToFile(string filename) {
+        using(StreamWriter entriesFile = new StreamWriter($"{filename}.txt")) {
+            foreach (Entry entry in _entries) {
+            entriesFile.WriteLine($"{entry._date}~{entry._promptText}~{entry._entryText}");
+            }
+        }
     }
 
-    public void LoadFromFile(string file) {
+    public void LoadFromFile(string filename) {
+        string[] lines = System.IO.File.ReadAllLines($"{filename}.txt");
 
+        foreach (string line in lines) {
+            string[] parts = line.Split("~");
+        }
     }
 }
