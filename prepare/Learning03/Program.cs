f333e992b17816090696ed501b9eec479efded62
guyzoch101 <tristanmanhei@gmail.com>
2024-05-06 18:21:29 -0600

    testing the 3 forms of fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // fraction 1: no parameters passed in
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDeciamlValue());
+
+        // fraction 2: a whole number (numerator = whole number, denominator = 1)
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDeciamlValue());
+
+        // fraction 3: a fraction
+        Fraction fraction3 = new Fraction(1, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDeciamlValue());
     }
 }
