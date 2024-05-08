b0e4d9d51aad67a8ac84462c2c543d1857291b6f
guyzoch101 <tristanmanhei@gmail.com>
2024-05-08 16:19:59 -0600

    structure created

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,23 @@
+public class Scripture {
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference Reference, string text) {
+
+    }
+
+    public void HideRandomWords(int numberToHide) {
+
+    }
+
+    public string GetDisplayText() {
+        return "";
+    }
+
+    public bool IsCompletelyHidden() {
+        
+        // placeholder
+        bool x = true;
+        return x;
+    }
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,27 @@
+public class Word {
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text) {
+
+    }
+
+    public void Hide() {
+
+    }
+
+    public void Show() {
+
+    }
+
+    public bool IsHidden() {
+
+        // placeholders
+        bool x = true;
+        return x;
+    }
+
+    public string GetDisplayText() {
+        return "";
+    }
+}
