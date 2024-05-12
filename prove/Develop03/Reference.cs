5c928d6f193cb4ef21e077918d326afffadfcc83
guyzoch101 <tristanmanhei@gmail.com>
2024-05-11 21:01:38 -0600

    reference class

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Reference {
     private string _book;
     private int _chapter;
@@ -5,14 +7,27 @@ public class Reference {
     private int _endVerse;
 
     public Reference(string book, int chapter, int verse) {
-
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = 0;
     }
 
     public Reference(string book, int chapter, int startVerse, int endVerse) {
-
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
     }
 
     public string GetDisplayText() {
-        return "";
+        if (_endVerse == 0) {
+            string display = $"{_book} {_chapter}:{_verse}";
+            return display;
+        }
+        else {
+            string display = $"{_book} {_chapter}:{_verse}-{_endVerse}";
+            return display;
+        }
     }
 }
