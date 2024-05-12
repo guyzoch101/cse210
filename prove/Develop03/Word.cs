8a1e1f1cd19849fdf2b5fece8290910598e3d180
guyzoch101 <tristanmanhei@gmail.com>
2024-05-11 22:16:27 -0600

    word class

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -3,25 +3,23 @@ public class Word {
     private bool _isHidden;
 
     public Word(string text) {
-
+        _text = text;
+        _isHidden = false;
     }
 
     public void Hide() {
-
+        _isHidden = true;
     }
 
     public void Show() {
-
+        _isHidden = false;
     }
 
     public bool IsHidden() {
-
-        // placeholders
-        bool x = true;
-        return x;
+        return _isHidden;
     }
 
     public string GetDisplayText() {
-        return "";
+        return _text;
     }
 }
