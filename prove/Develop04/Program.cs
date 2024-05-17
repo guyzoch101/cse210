bfcc051db9f20fc7a3e3fb2aae03c6258c3b3dc7
guyzoch101 <tristanmanhei@gmail.com>
2024-05-17 02:16:12 -0600

    Menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Mindfulness Activities");
+        string choice;
+
+        do {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Listing Activity");
+            Console.WriteLine("3. Reflecting Activity");
+            Console.WriteLine("4. Exit");
+            Console.Write("Please enter your choice: ");
+            choice = Console.ReadLine();
+
+            if (choice == "1") {
+
+            }
+            
+            else if (choice == "2") {
+
+            }
+
+            else if (choice == "3") {
+
+            }
+
+            else if (choice == "4") {
+                Console.WriteLine("Exiting...");
+            }
+            
+            else {
+                Console.WriteLine("Invalid choice entered.");
+            }
+
+        } while (choice != "4");
+
     }
 }
