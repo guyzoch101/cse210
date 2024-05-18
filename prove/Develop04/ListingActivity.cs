6541c7819da853cf18b7de176d45ba017c4ea71d
guyzoch101 <tristanmanhei@gmail.com>
2024-05-17 23:45:38 -0600

    ListingActivity class structure

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,20 @@
+public class ListingActivity : Activity {
+    private int _count;
+    List<string> _prompts = new List<string>();
+
+    public ListingActivity() : base() {
+
+    }
+
+    public void Run() {
+
+    }
+
+    public void GetRandomPrompt() {
+
+    }
+
+    public List<String> GetListFromUser() {
+        return _prompts;
+    }
+}
