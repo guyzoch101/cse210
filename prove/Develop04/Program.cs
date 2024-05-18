7711b0af1acf39edd48bc34a1a314d61d1e19396
guyzoch101 <tristanmanhei@gmail.com>
2024-05-18 02:52:16 -0600

    run functions to start the activities

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -18,16 +18,18 @@ class Program
             Console.WriteLine("");
 
             if (choice == "1") {
-                BreathingActivity breatheActivity = new BreathingActivity();
-                breatheActivity.Run();
+                BreathingActivity breathingActivity = new BreathingActivity();
+                breathingActivity.Run();
             }
             
             else if (choice == "2") {
-
+                ListingActivity listingActivity = new ListingActivity();
+                listingActivity.Run();
             }
 
             else if (choice == "3") {
-
+                ReflectingActivity reflectingActivity = new ReflectingActivity();
+                reflectingActivity.Run();
             }
 
             else if (choice == "4") {
