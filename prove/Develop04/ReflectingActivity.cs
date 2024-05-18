b216903dd945f9f05243f91dc965259cb8671935
guyzoch101 <tristanmanhei@gmail.com>
2024-05-17 23:45:49 -0600

    ReflectingActivity class structure

---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,28 @@
+public class ReflectingActivity : Activity {
+    List<string> _prompts = new List<string>();
+    List<string> _questions = new List<string>();
+
+    public ReflectingActivity() : base() {
+
+    }
+
+    public void Run() {
+
+    }
+
+    public string GetRandomPrompt() {
+        return "random prompt";
+    }
+
+    public string GetRandomQuestion() {
+        return "random question";
+    }
+
+    public void DisplayPrmopt() {
+
+    }
+
+    public void DisplayQuestions() {
+        
+    }
+}
