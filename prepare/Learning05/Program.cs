cc945e2efec9b7a4b3976816cd097db2266d1b6d
guyzoch101 <tristanmanhei@gmail.com>
2024-05-20 23:53:57 -0600

    Square class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square(10, "Red");
+        Console.WriteLine($"Color: {square.GetColor()}");
+        Console.WriteLine($"Area: {square.GetArea()}");
     }
 }
