eeb8b0df969bfe6083dee707970a2d3af0ea9807
guyzoch101 <tristanmanhei@gmail.com>
2024-05-25 22:39:33 -0600

    notes

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         GoalManager goalManager = new GoalManager();
-        goalManager.Start();
+        goalManager.Start(); // calling Start() to start everything
     }
 }
