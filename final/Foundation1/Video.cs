8b2d62c1c9f7fa9515df13d70c97c9ec15ffcd51
guyzoch101 <tristanmanhei@gmail.com>
2024-05-28 15:30:20 -0600

    Video class

---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,22 @@
+public class Video {
+    private string _title;
+    private string _author;
+    private int _lengthSec;
+    private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int lengthSec) {
+        _title = title;
+        _author = author;
+        _lengthSec = lengthSec;
+    }
+
+    public int GetNumberOfComments() {
+        int noOfComments = _comments.Count;
+        return noOfComments;
+    }
+
+    public void DisplayVideoDetails() {
+        string videoDetails = $"Title: {_title} ~ Author: {_author} ~ Length: {_lengthSec} seconds";
+        Console.WriteLine(videoDetails);
+    }
+}
