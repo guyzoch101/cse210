a605fc536c89c89cf2e869272c77224e12d70216
guyzoch101 <tristanmanhei@gmail.com>
2024-06-01 00:03:09 -0600

    Reception testing

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // lecture event
+        // Lecture Event
         Address addressLecture = new Address("256 Main Street", "Rexburg", "ID", "USA");
         Lecture lecture = new Lecture("STEM: the Future", "Importance of STEM Education", "3/3/2024", "10:00", addressLecture, "Harry Potter", 150);
 
@@ -14,5 +14,18 @@ class Program
         lecture.DisplayFullDetials();
         Console.WriteLine();
         Console.WriteLine(lecture.GetShortDescription(lecture.GetType().ToString()));
+
+        Console.WriteLine();
+
+        // Reception Event
+        Address addressReception = new Address("421 Wembley Street", "London", "England", "UK");
+        Reception reception = new Reception("23/24 FA Cup Finals Party", "Man Utd vs Man City", "5/25/2024", "15:00", addressReception, "manutd.fansclub@gmail.com");
+
+        // Display details
+        reception.DisplayStandardDetails();
+        Console.WriteLine();
+        reception.DisplayFullDetails();
+        Console.WriteLine();
+        Console.WriteLine(reception.GetShortDescription(reception.GetType().ToString()));
     }
 }
--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -1,16 +1,16 @@
 public class Reception : Event {
-    private RSVP _registration;
+    private string _registrationEmailAddress;
+    private string _eventType;
 
-    public Reception(string eventTitle, string description, string date, string time, Address address, RSVP registration)
+    public Reception(string eventTitle, string description, string date, string time, Address address, string registrationEmailAddress)
         : base(eventTitle, description, date, time, address) {
-        _registration = registration;
+        _registrationEmailAddress = registrationEmailAddress;
+        _eventType = "Reception";
     }
 
-    public string GetInvitationEmail() {
-        return "";
-    }
-
-    public string GetFullDetails() {
-        return "";
+    public void DisplayFullDetails() {
+        Console.WriteLine($"Type: {_eventType}");
+        DisplayStandardDetails();
+        Console.WriteLine($"Registration Email: {_registrationEmailAddress}");
     }
 }
