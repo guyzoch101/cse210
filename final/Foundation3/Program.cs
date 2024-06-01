e5fe149b02872b0e46fce896d0f9769730dd6ac4
guyzoch101 <tristanmanhei@gmail.com>
2024-05-31 23:52:32 -0600

    Testing for Lecture class

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // lecture event
+        Address addressLecture = new Address("256 Main Street", "Rexburg", "ID", "USA");
+        Lecture lecture = new Lecture("STEM: the Future", "Importance of STEM Education", "3/3/2024", "10:00", addressLecture, "Harry Potter", 150);
+
+        // Display details
+        lecture.DisplayStandardDetails();
+        Console.WriteLine();
+        lecture.DisplayFullDetials();
+        Console.WriteLine();
+        Console.WriteLine(lecture.GetShortDescription(lecture.GetType().ToString()));
     }
 }
