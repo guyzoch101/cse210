7ed88bf2d0668b6272cf4bbd6c97ff97b797b524
guyzoch101 <tristanmanhei@gmail.com>
2024-05-31 23:52:07 -0600

    Lecture class

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -1,14 +1,19 @@
 public class Lecture : Event {
     private string _speaker;
     private int _capacity;
+    private string _eventType;
 
     public Lecture(string eventTitle, string description, string date, string time, Address address, string speaker, int capacity)
         : base(eventTitle, description, date, time, address) {
         _speaker = speaker;
         _capacity = capacity;
+        _eventType = "Lecture";
     }
 
-    public string GetFullDetials() {
-        return "";
+    public void DisplayFullDetials() {
+        Console.WriteLine($"Type: {_eventType}");
+        DisplayStandardDetails();
+        Console.WriteLine($"Spaker: {_speaker}");
+        Console.WriteLine($"Capacitty: {_capacity}");
     }
 }
